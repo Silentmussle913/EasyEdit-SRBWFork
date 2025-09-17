a29750910d9dbad0dedbfbba84a64cce3629ceb4
Silentmussle913 <118747136+Silentmussle913@users.noreply.github.com>
2025-09-17 09:05:25 +0530

    Update Registry.php
    fixed it to not collide with newer blocks

--- src/platz1de/EasyEdit/world/clientblock/Registry.php
+++ src/platz1de/EasyEdit/world/clientblock/Registry.php
@@ -23,8 +23,9 @@ class Registry
 
 	protected static function setup(): void
 	{
-		self::_registryRegister("outline_block", new CompoundBlock(5, 0, CompoundTag::create()->setByte("showBoundingBox", 1)));
-		self::_registryRegister("window_block", new CompoundBlock(5, 5, CompoundTag::create()
+		// Use high, safe IDs instead of low ones (5,0) and (5,5)
+		self::_registryRegister("outline_block", new CompoundBlock(20001, 0, CompoundTag::create()->setByte("showBoundingBox", 1)));
+		self::_registryRegister("window_block", new CompoundBlock(20002, 0, CompoundTag::create()
 			->setString("id", "StructureBlock")
 			->setString("structureName", "clipboard")
 			->setString("dataField", "")
@@ -45,7 +46,7 @@ class Registry
 		 * @var Block $block
 		 */
 		foreach (self::_registryGetAll() as $block) {
-			//Note: not registering to Deserializer
+			// Note: not registering to Deserializer
 			GlobalBlockStateHandlers::getSerializer()->map($block, function (Block $block): BlockStateWriter {
 				if (!$block instanceof CompoundBlock) {
 					throw new BlockStateSerializeException("Cannot serialize " . get_class($block));
